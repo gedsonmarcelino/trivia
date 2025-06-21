649ac15962f5a9238238379ed3d6017dfddf1279
Gedson Marcelino <gedson.marcelino@gmail.com>
2025-06-21 18:05:13 -0400

    feat: added new images

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,9 +1,19 @@
 import ImageBack from './back.png'
+import ImageEntertainment from './entertainment.png'
+import ImageGeographt from './geography.png'
 import ImageGoogle from './google.png'
+import ImageHistory from './history.png'
 import ImageLogo from './logo.png'
+import ImageRandom from './random.png'
+import ImageScience from './science.png'
 
 export const Images = {
   logo: ImageLogo,
   google: ImageGoogle,
   back: ImageBack,
+  history: ImageHistory,
+  science: ImageScience,
+  random: ImageRandom,
+  entertainment: ImageEntertainment,
+  geography: ImageGeographt
 }
